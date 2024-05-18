25be18093012f20804114d4f78a22afff6f14d75
chusama24 <zenrox69@gmail.com>
2024-05-19 00:09:32 +0500

    Added Triadic Array

---
+++ src/data_structures/t_array.rs
@@ -0,0 +1,52 @@
+use crate::data_types::{triadic::Triadic, triadic_type::Ttypes};
+
+
+#[derive(Clone)]
+pub struct TArray<T: Ttypes>{
+    t_vector: Vec<T>,
+    degree: Triadic
+}
+
+impl<T: Ttypes> TArray<T>{
+
+   pub fn new(t_vec: Vec<T>, deg: Triadic) -> TArray<T>{
+        TArray{t_vector: t_vec, degree: deg}
+    }
+    
+    pub fn set_degree(&mut self, d: Triadic){
+        self.degree = d;
+    }
+
+    pub fn get_degree(&self)-> Triadic{
+        self.degree
+    }
+
+    pub fn get_vector(&self) -> Vec<T> {
+        self.t_vector.clone()
+    }
+
+    pub fn push(&mut self, val: T){
+        self.t_vector.push(val);
+    }
+    
+    pub fn pop(&mut self) -> Option<T> {
+       self.t_vector.pop()
+    }
+
+    pub fn insert_at(&mut self, val: T, index: usize){
+        self.t_vector.insert(index, val);
+    }
+
+    pub fn remove(&mut self, index: usize)-> T {
+        self.t_vector.remove(index)
+    }
+
+}
+
+impl<T: Ttypes> Default for TArray<T>{
+    fn default() -> Self {
+        Self{t_vector: Vec::new(), degree: Triadic::default()}
+    }
+}
+
+
